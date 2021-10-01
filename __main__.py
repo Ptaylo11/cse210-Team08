ec616108281a8064c00df41daad0f89ed06538bf
Marianne Johnson <joh20041@byui.edu>
2021-09-30 20:20:42 -0700

    merged packages and updated the main file.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,10 @@
-#Intro text
+#Import greeting functions
 from greetings.sanders import display_stephen
 from mjgreeting.johnson import greeting
 
+#Intro
 print('Welcome to our collaborative program.')
+
+#Functions
 display_stephen()
 greeting()
