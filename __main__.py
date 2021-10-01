ddf244925f5bda762106931f0f560a821468dfb3
Marianne Johnson <joh20041@byui.edu>
2021-09-30 20:14:42 -0700

    Updated package and added my greeting function to the main file.

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 #Intro text
-import greetings
 from greetings.sanders import display_stephen
+from mjgreeting.johnson import greeting
 
 print('Welcome to our collaborative program.')
 display_stephen()
+greeting()
